33f88629a0a1a3e525b55d9511d4ed9d4468c890
Hibecode <ibkokunoye@gmail.com>
2022-03-09 11:30:59 +0100

    Initial commit

---
+++ lib/profile_page.dart
@@ -0,0 +1,62 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:profilepage/appbar_widget.dart';
+import 'package:profilepage/model/user.dart';
+import 'package:profilepage/utils/user_pref.dart';
+import 'package:profilepage/widget/button_widget.dart';
+import 'package:profilepage/widget/profile_widget.dart';
+
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    final user = UserPrefernces.myUser;
+
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          ProfileWidget(
+            imagePath: user.imagePath,
+            onClicked: () async {},
+          ),
+          const SizedBox(height: 24),
+          buildName(user),
+          const SizedBox(height: 24),
+          Center(child: buildUpgradeButton()),
+        ],
+      )
+    );
+  }
+
+
+  Widget buildName(User user) => Column(
+    children: [
+      Text(
+        user.name,
+        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)
+      ),
+      const SizedBox(height: 4),
+      Text(
+        user.email,
+        style: const TextStyle(color: Colors.blueGrey),
+      )
+    ],
+  );
+
+  Widget buildUpgradeButton() => ButtonWidget(
+    text: 'Upgrade To PRO',
+    onClicked: () {},
+  );
+
+
+
+}
---
+++ lib/utils/user_pref.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:profilepage/model/user.dart';
+
+class UserPrefernces {
+  static const myUser = User(
+    imagePath: 'https://yt3.ggpht.com/a/AATXAJx36UVD6WuyuRqbHcVK9RPzKzK3Gh5XYqx_Sw=s900-c-k-c0xffffffff-no-rj-mo',
+  name: 'Eren Jaegar',
+  email: 'freeeldians@gmail.com',
+  about: 'Holder of the Attack titan',
+  isDarkMode: false);
+
+}
